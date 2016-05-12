4c55099f912ab5d3ec97a3c5e5e6c6b2de33f4b8
Christopher Soden <karavol27@gmail.com>
2016-05-11 20:33:03 -0600

    Create Categories Table

---
+++ db/migrate/20160512022250_add_column_to_photos.rb
@@ -0,0 +1,5 @@
+class AddColumnToPhotos < ActiveRecord::Migration
+  def change
+    add_reference :photos, :category, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511192028) do
+ActiveRecord::Schema.define(version: 20160512022110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "destinations", force: :cascade do |t|
     t.string   "planet"
