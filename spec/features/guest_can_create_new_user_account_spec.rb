d2b4fb355bb2a156ad6209427eefd4763c4fbd39
Christopher Soden <karavol27@gmail.com>
2016-05-12 16:25:18 -0600

    Add sad path tests for failure to create user account

--- spec/features/guest_can_create_new_user_account_spec.rb
+++ spec/features/guest_can_create_new_user_account_spec.rb
@@ -21,18 +21,60 @@ RSpec.feature "guest can create a new user account" do
 
     expect(current_path).to eq(login_path)
     expect(page).to have_content("Success! Your account was created!.")
+    expect(User.count).to eq(original_user_count + 1)
   end
-  #   expect(page).to have_content(vendor.name)
-  #   expect(page).to have_xpath("//img[@src=\"#{photo.image}\"]")
-  #   expect(page).to have_link(photo.image.url, href: photo_path(photo))
-  # end
-  #
-  # scenario "guest doesn't see pictures for a vendor that doesn't exist" do
-  #   vendor = Vendor.create(name: "vendor")
-  #   photo = vendor.photos.create(title: "photo", image: Photo.image_from_url("https://source.unsplash.com/random"), price: 20, description: "description")
-  #
-  #   visit "/something-else"
-  #
-  #   expect(page).to have_content('404')
-  # end
+
+  scenario "guest trys to create account without username" do
+    original_user_count = User.count
+
+    visit users_path
+
+    expect(current_path).to eq(users_path)
+
+    fill_in "Email", with: "j1@gmail.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Create Account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Your account could not be created. Please check your input and try again.")
+    expect(User.count).to eq(original_user_count)
+  end
+
+  scenario "guest trys to create account without email" do
+    original_user_count = User.count
+
+    visit users_path
+
+    expect(current_path).to eq(users_path)
+
+    fill_in "username", with: "JoeSmith"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Create Account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Your account could not be created. Please check your input and try again.")
+    expect(User.count).to eq(original_user_count)
+  end
+
+  scenario "guest trys to create account without password" do
+    original_user_count = User.count
+
+    visit users_path
+
+    expect(current_path).to eq(users_path)
+
+    fill_in "username", with: "JoeSmith"
+    fill_in "Email", with: "j1@gmail.com"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Create Account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Your account could not be created. Please check your input and try again.")
+    expect(User.count).to eq(original_user_count)
+  end
 end
