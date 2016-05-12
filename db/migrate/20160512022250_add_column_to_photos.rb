56aaaf70748f10442b60ce46bf41ca5951eff07e
Christopher Soden <karavol27@gmail.com>
2016-05-11 20:35:40 -0600

    Remove bad migration

--- db/migrate/20160512022250_add_column_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPhotos < ActiveRecord::Migration
-  def change
-    add_reference :photos, :category, index: true, foreign_key: true
-  end
-end
---
+++ spec/factories/photo_categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :photo_category do
+    photo nil
+    category nil
+  end
+end
