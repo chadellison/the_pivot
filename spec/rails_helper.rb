9cc8aaeafa8d0dd1566e9a76912399d099468482
Christopher Soden <karavol27@gmail.com>
2016-05-14 18:28:19 -0600

    Added SimpleCov to the project

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts "required simplecov"
+end
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
