0625b24f966dcc97d47771d271ec99ea854b8ce8
Jbern16 <jbern16@gmail.com>
2016-05-18 15:11:15 -0600

    Download Photo on Production Functional

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,7 @@
 class DownloadsController < ApplicationController
   def show
     photo = Photo.find(params[:photo])
-    send_file photo.image.path, type: "image/jpeg"
+    data = open(photo.image.url)
+    send_data data.read, :type => data.content_type, :x_sendfile => true
   end
 end
