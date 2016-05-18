190894b9608fd55c39ea21f3572364d66d85f61f
Sal Espinosa <sespinos@gmail.com>
2016-05-18 11:56:53 -0600

    Add ability for admin to add another admin

--- app/assets/javascripts/vendor_form.js
+++ app/assets/javascripts/vendor_form.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var vendor = this.id;
     $(".user-form").addClass("hidden")
     $(".vendor-form").addClass("hidden")
+    $(".vendor-admin-form").addClass("hidden")
     $("#" + vendor + "-form").toggleClass("hidden")
     $("h5").addClass("hidden")
     $("h6").addClass("hidden")
@@ -10,9 +11,20 @@ $(document).ready(function() {
 
   $(".side-menu").on("click", "#edit-profile", function() {
     $(".vendor-form").addClass("hidden")
+    $(".vendor-admin-form").addClass("hidden")
     $(".recent-orders").addClass("hidden")
     $(".new-business").addClass("hidden")
     $(".user-form").toggleClass("hidden")
+    $("h5").addClass("hidden")
+    $("h6").addClass("hidden")
+  })
+
+  $(".side-menu").on("click", ".add-admin", function() {
+    $(".user-form").addClass("hidden")
+    $(".vendor-form").addClass("hidden")
+    $(".recent-orders").addClass("hidden")
+    $(".new-business").addClass("hidden")
+    $(".vendor-admin-form").toggleClass("hidden")
     $("h5").addClass("hidden")
     $("h6").addClass("hidden")
   })
--- spec/features/vendor_can_add_an_admin_spec.rb
+++ spec/features/vendor_can_add_an_admin_spec.rb
@@ -2,38 +2,31 @@ require 'rails_helper'
 
 RSpec.feature "Vendor can add an admin" do
   scenario "a vendor adds a new admin" do
-    # As a Vendor Admin
     admin  = User.create(username: "test", password: "pass", password_confirmation: "pass", email: "test@test.com")
     role   = Role.create(name: "vendor_admin")
     vendor = Vendor.create(name: "Vendor1", status: 1)
     UserRole.create(user: admin, vendor: vendor, role: role)
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
-    # When I visit my dashboard
     visit("/dashboard/#{admin.id}")
-    # and click on Add Admin
-    click_on("Add Admin")
-    # And fill in the form  fields for the account information of a new admin
-    save_and_open_page
-    within(".vendor-admin-form") do
+    within(".side-menu") do
+      click_on("Add Admin")
+    end
+    within(".create-admin-form") do
       fill_in "user[username]", with: "username"
       fill_in "user[email]", with: "e@mail.com"
       fill_in "user[password]", with: "password"
       fill_in "user[password_confirmation]", with: "password"
+      click_on "Create Admin"
     end
-    # And I click on "Create Admin"
-    click_on "Create Admin"
 
-    # I expect to see a confirmation page stating the admin was created.
     vendor = Vendor.first
-    expect(page).to have_content("Admin successfully created")
-    # And I expect for a new admin associated with my store to have been created.
     expect(vendor.users.count).to eq(2)
-    expect(vendor.users.last).to eq(user)
+    expect(vendor.users.last.roles.last.name).to eq("vendor_admin")
+    expect(page).to have_content("Admin successfully created.")
   end
 
   scenario " a vendor adds an existing user as an admin" do
-    # As a Vendor Admin
     admin  = User.create(username: "test", password: "pass", password_confirmation: "pass", email: "test@test.com")
     user   = User.create(username: "user", password: "pass", password_confirmation: "pass", email: "email@email.com")
     role   = Role.create(name: "vendor_admin")
@@ -41,20 +34,19 @@ RSpec.feature "Vendor can add an admin" do
     UserRole.create(user: admin, vendor: vendor, role: role)
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
-    # When I visit my dashboard
     visit("/dashboard/#{admin.id}")
-    # and click on Add Admin
-    click_on("Add Admin")
-    # And fill in a form with an existing user name
-    fill_in "vendor_user[name]", with: "New Admin Name"
-    # And I click on "Add Admin"
-    click_on "Add Admin"
+    within(".side-menu") do
+      click_on("Add Admin")
+    end
+    within(".add-admin-form") do
+      fill_in "user[username]", with: "user"
+      click_on "Add Admin"
+    end
 
-    # I expect to see a confirmation page stating the admin was created.
-    expect(page).to have_content("Admin successfully created")
-    # And I expect for a new admin associated with my store to have been created.
     vendor = Vendor.first
     expect(vendor.users.count).to eq(2)
-    expect(vendor.users.last).to eq(user)
+    expect(vendor.users.last.username).to eq("user")
+    expect(vendor.users.last.roles.last.name).to eq("vendor_admin")
+    expect(page).to have_content("Admin successfully created.")
   end
 end
