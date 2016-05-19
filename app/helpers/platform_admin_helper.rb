612387c2668fc84c7e7f7c039a0aa0886e093723
Chad Ellison <chad.ellison0123@gmail.com>
2016-05-19 08:59:26 -0600

    Remove platform helper method

--- app/helpers/platform_admin_helper.rb
+++ app/helpers/platform_admin_helper.rb
@@ -1,9 +1,4 @@
 module PlatformAdminHelper
-  def most_popular_photos
-    photo_ids = OrderPhoto.pluck(:photo_id)
-    photo_ids.map { |id| Photo.find(id) }.group_by(&:title)
-  end
-
   def user_demographics
     User.group(:country).count
   end
